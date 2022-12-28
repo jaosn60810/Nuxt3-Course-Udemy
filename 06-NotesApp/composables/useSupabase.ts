7510361002682862a70f2ca0782e27b4598b721a
jaosn60810 <jason60810@gmail.com>
2022-12-28 22:11:00 +0800

    s22

--- 06-NotesApp/composables/useSupabase.ts
+++ 06-NotesApp/composables/useSupabase.ts
@@ -1,8 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhyeGVib2NoanFtaXFycnFycXF2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1MDIxMzI1MCwiZXhwIjoxOTY1Nzg5MjUwfQ.w30KrvbS3krHbHvhUsFTX-ag0DX2RgSGaivSE4KPRDA";
-const SUPABASE_URL = "https://xrxebochjqmiqrrqrqqv.supabase.co";
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhyeGVib2NoanFtaXFycnFycXF2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1MDIxMzI1MCwiZXhwIjoxOTY1Nzg5MjUwfQ.w30KrvbS3krHbHvhUsFTX-ag0DX2RgSGaivSE4KPRDA';
+const SUPABASE_URL = 'https://xrxebochjqmiqrrqrqqv.supabase.co';
 
 const useSupabase = () => {
   const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
@@ -13,3 +13,4 @@ const useSupabase = () => {
 };
 
 export default useSupabase;
+// s22
